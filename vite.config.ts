3c2a14b47f6f0d6298247a5123b4d4e654816c72
vikas-011 <vikas.tech011@gmail.com>
2025-12-23 22:37:19 +0530

    do some new chnages

--- vite.config.ts
+++ vite.config.ts
@@ -43,6 +43,12 @@ export default defineConfig({
   },
   server: {
     host: "0.0.0.0",
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
     allowedHosts: true,
     fs: {
       strict: true,
